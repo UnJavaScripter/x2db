b27c11927556c1e111897f48bd022b0dc0fc2bc2
Diego Coy <diegoct11@hotmail.com>
2014-11-05 22:04:33 -0500

    Updated UI

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var codeInput = document.getElementById("codeInput"),
+	result = document.getElementById("result"),
+	datatypes = [
+		{'languageType':'string', 'databaseType': 'nvarchar(max)'},
+		{'languageType':'Byte', 'databaseType': 'tinyint'},
+		{'languageType':'short', 'databaseType': 'smallint'},
+		{'languageType':'int', 'databaseType': 'int'},
+		{'languageType':'Int64', 'databaseType': 'bigint'},
+		{'languageType':'bool', 'databaseType': 'bit'},
+		{'languageType':'float', 'databaseType': 'real'},
+		{'languageType':'double', 'databaseType': 'float'},
+		{'languageType':'DateTime', 'databaseType': 'datetime'},
+		{'languageType':'TimeSpan', 'databaseType': 'time'},
+		{'languageType':'Guid', 'databaseType': 'uniqueidentifier'}
+	],
+	findDataType = function(dataType){
+		for(var i = 0; i < datatypes.length; i++) {
+		   if(datatypes[i].languageType === dataType) {
+		     return datatypes[i].databaseType;
+		   }
+		}
+	};
+
+function x2db() {
+	var inputValue = codeInput.value,
+		allTheLines = inputValue.split("\n"),
+		lines = [],
+		resultLines = [];
+
+	for(aLine in allTheLines){
+		lines[aLine] = allTheLines[aLine].split(" ");
+		resultLines[aLine] = [];
+	}
+
+	for(var i = 0 ; i < lines.length ; i++){
+		if(lines[i][0] === "public"){
+			resultLines[i][0] = lines[i][2] + " " + findDataType(lines[i][1]);
+			if (lines[i][1].indexOf('?') === -1){
+				if( (i+1) == lines.length){
+					resultLines[i][0] += " not null";
+				}else{
+					resultLines[i][0] += " not null,";
+				}
+			}
+			else{
+				resultLines[i][0] += ",";    
+			}
+		}else{
+			resultLines[i][0] = "" + lines[i][0];
+		}
+	}
+	result.innerText = resultLines.join("\n");
+
+};
+function focus_blur(elem){
+	var this_pre = elem.parentNode;
+	var resultContainer = this_pre.parentNode;
+	resultContainer.classList.toggle('level5Shadow')
+}
+codeInput.addEventListener ("paste", x2db, false);
